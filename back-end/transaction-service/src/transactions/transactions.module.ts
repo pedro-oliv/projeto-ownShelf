3e4284fa712bbcf4227f8ba38e14db47fddafa6a
Pedro Oliveira <limapedro025@gmail.com>
2026-06-02 01:21:34 -0300

    ajustes deploy

--- back-end/transaction-service/src/transactions/transactions.module.ts
+++ back-end/transaction-service/src/transactions/transactions.module.ts
@@ -3,29 +3,12 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 
 @Module({
   imports: [
     PrismaModule,
 
-    ClientsModule.register([
-      {
-        name: 'RABBIT_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            process.env.RABBITMQ_URL ||
-              'amqp://guest:guest@localhost:5672',
-          ],
-          queue: 'library_queue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
   ],
 
   controllers: [TransactionsController],
